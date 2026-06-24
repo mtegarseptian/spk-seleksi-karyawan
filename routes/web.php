971ceed5e8e3b4f109fd20b777b1b261eb1a8552
mtegarseptian <mtegarseptian@gmail.com>
2026-06-24 20:25:59 +0700

    feat: tambah routing seluruh halaman

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AhpController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DatasetController;
+use App\Http\Controllers\HasilSpkController;
+use App\Http\Controllers\RandomForestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/dataset', [DatasetController::class, 'index'])->name('dataset.index');
+Route::post('/dataset/import', [DatasetController::class, 'import'])->name('dataset.import');
+
+Route::get('/ahp', [AhpController::class, 'index'])->name('ahp.index');
+Route::post('/ahp/hitung', [AhpController::class, 'hitung'])->name('ahp.hitung');
+
+Route::get('/random-forest', [RandomForestController::class, 'index'])->name('random-forest.index');
+Route::post('/random-forest/train', [RandomForestController::class, 'train'])->name('random-forest.train');
+
+Route::get('/hasil-spk', [HasilSpkController::class, 'index'])->name('hasil-spk.index');
+Route::post('/hasil-spk/proses', [HasilSpkController::class, 'proses'])->name('hasil-spk.proses');
+Route::get('/hasil-spk/export', [HasilSpkController::class, 'export'])->name('hasil-spk.export');
